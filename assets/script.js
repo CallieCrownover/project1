b70a1c921b0b86b38a4bbc5a3ce3dc8c6a88418b
jdsheridan02 <jdsheridan02@gmail.com>
2020-06-06 13:02:51 -0400

    updated script with additional API, as well as, variables for each card

--- assets/script.js
+++ assets/script.js
@@ -15,30 +15,121 @@ $("#foodSearch").on("click", function(event) {
 	var settings = {
 		"async": true,
 		"crossDomain": true,
-		"url": "https://api.spoonacular.com/recipes/762009/information?includeNutrition=false&apiKey=380aee2f63msh502b64f71be7614p164be7jsn561a2dd539f1" ,
-		"method": "GET"
-		// "headers": {
-		// 	"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-		// 	"Access-Control-Allow-Origin": "*",
-		// 	"x-rapidapi-key": "380aee2f63msh502b64f71be7614p164be7jsn561a2dd539f1"
-		// }
+		"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=" + foodIng,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+			"x-rapidapi-key": "380aee2f63msh502b64f71be7614p164be7jsn561a2dd539f1"
+		}
 	}
 
 	$.ajax(settings).done(function (response) {
 		console.log(response);
 
-		//will need to display of results to card, need to pull the image from the response, dynamically create element for image, set attribute and place the image to HTML
-		var dishImg = response.[i].image
+		// variable that pulls info from API (image, title, id)
+		var dishOneImg = response[0].image;
+		var dishOneTitle = response[0].title;
+		var dishId = response[0].id;
+		
+		var dishTwoImg = response[1].image;
+		var dishTwoTitle = response[1].title;
+		var dishTwoId = response[1].id;
 
-		//this API only provides dish name and ingredients necessary to make the dish, but no step by step instruction, creating a variable to store the dish name to call another API to pull the recipe and step by step instrcutions
-		var dishName = response.[i].title
+		var dishThreeImg = response[2].image;
+		var dishThreeTitle = response[2].title;
+		var dishThreeId = response[2].id;
 
-		//function for the new API pull, will need to build the new function outside of this current function
-		searchRecipe(dishName);
+		var dishFourImg = response[3].image;
+		var dishFourTitle = response[3].title;
+		var dishFourId = response[3].id;
+
+		var dishFiveImg = response[4].image;
+		var dishFiveTitle = response[4].title;
+		var dishFiveId = response[4].id;
+
+		var dishSixImg = response[5].image;
+		var dishSixTitle = response[5].title;
+		var dishSixId = response[5].id;
+
+		var dishSevenImg = response[6].image;
+		var dishSevenTitle = response[6].title;
+		var dishSevenId = response[6].id;
+
+		var dishEightImg = response[7].image;
+		var dishEightTitle = response[7].title;
+		var dishEightId = response[7].id;
+
+		//create element and adds attribute
+		var dishImg = $("<img>");
+		dishImg.attr("src", dishOneImg);
+
+		var dishImgTwo = $("<img>");
+		dishImgTwo.attr("src", dishTwoImg);
+
+		var dishImgThree = $("<img>");
+		dishImgThree.attr("src", dishThreeImg);
+
+		var dishImgFour = $("<img>");
+		dishImgFour.attr("src", dishFourImg);
+
+		var dishImgFive = $("<img>");
+		dishImgFive.attr("src", dishFiveImg);
+
+		var dishImgSix = $("<img>");
+		dishImgSix.attr("src", dishSixImg);
+
+		var dishImgSeven = $("<img>");
+		dishImgSeven.attr("src", dishSevenImg);
+
+		var dishImgEight = $("<img>");
+		dishImgEight.attr("src", dishEightImg);
+
+
+		//placing information to the HTML page
+		$("#cardOne").append(dishImg);
+		$("#titleOne").append(dishOneTitle);
+
+		// for (var i=0;i<4;i++){
+		// 	var img =$("#imge"+i+"")
+		// 	img.attr("src",response[i].image)
+
+
+
+
+
+		// 	console.log(response[i].id);
+		// 	console.log(response[i].image);
+
+		// }
+
+		// //will need to display of results to card, need to pull the image from the response, dynamically create element for image, set attribute and place the image to HTML
+		// var dishImg = response.[i].image
+
+		// //this API only provides dish name and ingredients necessary to make the dish, but no step by step instruction, creating a variable to store the dish name to call another API to pull the recipe and step by step instrcutions
+		// var dishName = response.[i].title
+
+		// //function for the new API pull, will need to build the new function outside of this current function
+		searchRecipe(dishId);
 	});
-
-
 });
+function searchRecipe(dishId){
+	
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + dishId + "/information",
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+			"x-rapidapi-key": "380aee2f63msh502b64f71be7614p164be7jsn561a2dd539f1"
+		}
+	}
+	
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+};
+
 // on click event to initiate cocktail search once ingredients are entered
 $("#drinkSearch").on("click", function(event){ 
 
@@ -51,7 +142,7 @@ $("#drinkSearch").on("click", function(event){
 	var cocktail = {
 		"async": true,
 		"crossDomain": true,
-		"url": "https://the-cocktail-db.p.rapidapi.com/search.php?i=" + drinkIng,
+		"url": "https://the-cocktail-db.p.rapidapi.com/filter.php?i=" + drinkIng,
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
@@ -62,6 +153,22 @@ $("#drinkSearch").on("click", function(event){
 	$.ajax(cocktail).done(function (response) {
 		console.log(response);
 		console.log("****")
-	});
+
+	// 	var cocktailOne = response.drinks.0.idDrink
+	// });
 	});
 
+	// var coctailRecipe = {
+	// 	"async": true,
+	// 	"crossDomain": true,
+	// 	"url": "https://the-cocktail-db.p.rapidapi.com/lookup.php?i=3",
+	// 	"method": "GET",
+	// 	"headers": {
+	// 		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+	// 		"x-rapidapi-key": "380aee2f63msh502b64f71be7614p164be7jsn561a2dd539f1"
+	// 	}
+	// }
+	
+	// $.ajax(cocktailR).done(function(response) {
+	// 	console.log(response);
+	});
